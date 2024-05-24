a0386589a641548996254255bfc9e30e27dfa22e
LuisM <118645322+MGLviS@users.noreply.github.com>
2024-05-24 02:21:17 -0500

    Pantalla Resumen Reserva

---
+++ app/src/main/java/com/example/turismogodpa/ui/user/reservas/ConfirmaReservaActivity.kt
@@ -0,0 +1,52 @@
+package com.example.turismogodpa.ui.user.reservas
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.turismogodpa.R
+import com.example.turismogodpa.ui.autentication.LoginActivity
+
+class ConfirmaReservaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_confirma_reserva)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val ivVolver: ImageView = findViewById(R.id.ivVolver)
+        val tvVolver: TextView = findViewById(R.id.tvVolver)
+        val tvRRIdUsuario: TextView = findViewById(R.id.tvRRIdUsuario)
+        val tvRRNombreContacto: TextView = findViewById(R.id.tvRRNombreContacto)
+        val tvRRActividad: TextView = findViewById(R.id.tvRRActvidad)
+        val tvRREmpresa: TextView = findViewById(R.id.tvRREmpresa)
+        val tvRRFecha: TextView = findViewById(R.id.tvRRFecha)
+
+        ivVolver.setOnClickListener {
+            BackMenu()
+        }
+        tvVolver.setOnClickListener {
+            BackMenu()
+        }
+
+        tvRRIdUsuario.text = "pguerrero@peru.com"
+        tvRRNombreContacto.text = "Paolo Guerrero"
+        tvRRActividad.text = "Caminata"
+        tvRREmpresa.text = "Turismo Peru"
+        tvRRFecha.text = "2021-09-30"
+
+    }
+
+    private fun BackMenu() {
+        val  intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
