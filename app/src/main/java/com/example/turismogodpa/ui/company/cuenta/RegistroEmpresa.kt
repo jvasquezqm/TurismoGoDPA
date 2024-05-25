b965fe1f71c05e1c480f7bf1d258845582df35ea
wramos98 <wilfredo.ramos@tecsup.edu.pe>
2024-05-24 21:58:10 -0500

    Avance 1 - pantallas historias de usuario wramos

---
+++ app/src/main/java/com/example/turismogodpa/ui/company/cuenta/RegistroEmpresa.kt
@@ -0,0 +1,21 @@
+package com.example.turismogodpa.ui.company.cuenta
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.turismogodpa.R
+
+class RegistroEmpresa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro_empresa)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
