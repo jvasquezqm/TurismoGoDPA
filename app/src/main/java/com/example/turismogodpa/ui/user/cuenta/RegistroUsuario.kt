28b6d6a72ba1591a22704ed2f71aa5c6f678bda5
wramos98 <wilfredo.ramos@tecsup.edu.pe>
2024-07-05 11:03:52 -0500

    Commit Cambio variables campos registro usuario-

--- app/src/main/java/com/example/turismogodpa/ui/user/cuenta/RegistroUsuario.kt
+++ app/src/main/java/com/example/turismogodpa/ui/user/cuenta/RegistroUsuario.kt
@@ -3,12 +3,15 @@ package com.example.turismogodpa.ui.user.cuenta
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.turismogodpa.R
 import com.example.turismogodpa.R.id.btRegistroUsuario
+import com.example.turismogodpa.R.id.etNombreUsuarioReg
 import com.google.android.material.snackbar.Snackbar
 
 class RegistroUsuario : AppCompatActivity() {
@@ -23,7 +26,16 @@ class RegistroUsuario : AppCompatActivity() {
             insets
         }
 
+        val etNombreUsuarioReg: EditText = findViewById(R.id.etNombreUsuarioReg)
+        val etApellidoUsuarioReg: EditText = findViewById(R.id.etApellidoUsuarioReg)
+        val etDniUsuarioReg: EditText = findViewById(R.id.etDniUsuarioReg)
+        val etCorreoUsuarioReg: EditText = findViewById(R.id.etCorreoUsuarioReg)
+        val etFechaNacimientoUsuarioReg: EditText = findViewById(R.id.etFechaNacimientoUsuarioReg)
+        val etTelefonoUsuarioReg: EditText = findViewById(R.id.etTelefonoUsuarioReg)
+        val etPasswordUsuarioReg: EditText = findViewById(R.id.etPasswordUsuarioReg)
+        val etPassword2UsuarioReg: EditText = findViewById(R.id.etPassword2UsuarioReg)
         val btRegistroUsuario : Button = findViewById(R.id.btRegistroUsuario)
+
 
         btRegistroUsuario.setOnClickListener{
             val rootView : View = findViewById(android.R.id.content)
