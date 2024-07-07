9d50b9fb6e2227917591418d08e3be363ca2259e
Jonathan <jvasquezqm@gmail.com>
2024-07-07 12:02:48 -0500

    publicación

--- app/src/main/java/com/example/turismogodpa/ui/company/publicacion/PublicacionFragment.kt
+++ app/src/main/java/com/example/turismogodpa/ui/company/publicacion/PublicacionFragment.kt
@@ -7,17 +7,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.turismogodpa.AddPubEmpActivity
 import com.example.turismogodpa.R
 import com.example.turismogodpa.adapter.PubAdapter
 import com.example.turismogodpa.data.PubResumData
+import com.example.turismogodpa.data.model.UserProfile
+import com.example.turismogodpa.ui.autentication.dataStore
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 class PublicacionFragment : Fragment(), PubAdapter.OnImageButtonClickListener {
-
+    private var idCompany: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,29 +37,35 @@ class PublicacionFragment : Fragment(), PubAdapter.OnImageButtonClickListener {
         val rvPublicaciones: RecyclerView = view.findViewById(R.id.rvPublicaciones)
         var pubList: List<PubResumData>
 
+        runBlocking {
+            idCompany = getUserProfile().first().userId
+
+        }
+        println("IDCOMPANY $idCompany")
+        val companyRef: DocumentReference = db.collection("users").document(idCompany!!)
+
         db.collection("activities")
             .orderBy("titulo")
             .whereEqualTo("state", "Activo")
-            .addSnapshotListener{ snap, error ->
-                if (error!=null){
+            .whereEqualTo("idCompany", companyRef)
+            .addSnapshotListener { snap, error ->
+                if (error != null) {
                     Log.e("ERROR-FIREBASE", "Detalle del error: ${error.message}")
                     return@addSnapshotListener
                 }
 
-                pubList = snap!!.documents.map{document ->
+                pubList = snap!!.documents.map { document ->
                     PubResumData(
                         document["image"].toString(),
                         document["titulo"].toString(),
                         document["description"].toString(),
                         document.id,
                         document["state"].toString()
-
                     )
                 }
                 rvPublicaciones.adapter = PubAdapter(pubList, this)
                 rvPublicaciones.layoutManager = LinearLayoutManager(requireContext())
             }
-
 
         // Configurar el OnClickListener para el botón flotante
         btAddPub.setOnClickListener {
@@ -60,14 +73,20 @@ class PublicacionFragment : Fragment(), PubAdapter.OnImageButtonClickListener {
             startActivity(intent)
         }
 
-
         return view
-
     }
+
     override fun onImageButtonClick(documentId: String) {
         val intent = Intent(requireContext(), UpdatePubEmpActivity::class.java)
         intent.putExtra("DOCUMENT_ID", documentId)
         startActivity(intent)
     }
 
+    private fun getUserProfile() = requireContext().dataStore.data.map { preferences ->
+        UserProfile(
+            name = preferences[stringPreferencesKey("name")].orEmpty(),
+            email = preferences[stringPreferencesKey("email")].orEmpty(),
+            userId = preferences[stringPreferencesKey("userId")].orEmpty()
+        )
+    }
 }
